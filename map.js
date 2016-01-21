707a326a5e02211d04ea71fb20dc01a3fa332212
arvidnorlin <arvidnorlin@gmail.com>
2016-01-21 10:54:21 +0100

    changed zoom level in map.js. tidied up style.css and removed unnecessary rules.

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ function initMap() {
   var myLatLng = {lat: 59.345300, lng: 18.023515};
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
+    zoom: 12,
     center: myLatLng,
 	disableDefaultUI: true,
 	scrollwheel: false
